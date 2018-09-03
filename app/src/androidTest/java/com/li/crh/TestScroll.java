ee83173ea0e26418712a5e3b8fb8bbe991b7c0cd
减了肥再改昵称 <770505210@qq.com>
2018-09-03 13:58:02 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/li/crh/TestScroll.java
@@ -0,0 +1,16 @@
+package com.li.crh;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class TestScroll {
+
+    @Test
+    public void testmode(){
+
+
+    }
+}
